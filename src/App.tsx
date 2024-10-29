02089007f700596bc0c1a4b12a48f6b3a4651f46
linhtran <linh.tran@saleshood.com>
2024-10-29 17:10:58 +0700

    chore: remove unused vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, HashRouter as Router } from "react-router-dom";
+import { Route, Routes, HashRouter as Router } from "react-router-dom";
 
 import "./App.css";
 import PortfolioPage from "./pages/portfolio";
