9f033a37b271af0c438db7ffd0ad6ad2b656d21f
linhtran <linh.tran@saleshood.com>
2024-10-29 18:11:16 +0700

    feat: WIP build terminal page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
-import "./App.css";
 import PortfolioPage from "./pages/portfolio";
 import TerminalPage from "./pages/terminal";
 
 function App() {
   return (
     <>
-    <h1>hello</h1>
       <Router>
         <Routes>
           <Route path="/" element={<TerminalPage />} />
