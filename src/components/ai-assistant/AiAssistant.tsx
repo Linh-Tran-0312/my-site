8b36ec840efd62fff431cc2e9ba5b49e19e7da31
linhtran <linh.tran@saleshood.com>
2025-05-17 00:06:12 +0700

    feat: WIP chat assistant

--- src/components/ai-assistant/AiAssistant.tsx
+++ src/components/ai-assistant/AiAssistant.tsx
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
-import ChatBox from './ChatBox';
+import ChatBox from './ChatMode';
 import './AiAssistant.css';
 import { Button } from 'react-bootstrap';
-const src =
-  'https://cdn.pixabay.com/animation/2024/07/19/00/09/00-09-13-492_512.gif';
-const srcActive =
-  'https://cdn.pixabay.com/animation/2024/07/19/00/09/00-09-35-674_512.gif';
-const MODE = {
-  SLEEPING: 'sleeping',
-  GREETING: 'greeting',
-  CHATTING: 'chatting',
-  BYE: 'bye',
+import Cat from './Cat';
+import GreetingMode from './GreetingMode';
+import { MODE } from './constant';
+
+const verCatSrc = {
+  static: 'chat/static-cat-1.png',
+  active: 'chat/active-cat-1.gif',
 };
+
 function AiAssistant() {
   const [mode, setMode] = useState(MODE.SLEEPING);
   const [greeted, setGreeted] = useState(false);
+
   const handleClick = () => {
     if (mode === MODE.SLEEPING) {
       if (greeted) {
@@ -28,72 +28,26 @@ function AiAssistant() {
       setMode(MODE.SLEEPING);
     }
   };
-  const handleSayYes = () => {
-    setMode(MODE.CHATTING);
-    setGreeted(true);
-  };
-  return (
-    <div className=''>
-      {![MODE.BYE, MODE.SLEEPING].includes(mode) ? (
-        <div
-          className={`chat-container border ${mode === MODE.CHATTING ? 'active-chat' : ''}`}
-        >
-          <div className='position-relative'>
-            <div
-              className={mode === MODE.CHATTING ? 'cat-active-chat' : 'd-none'}
-              onClick={() => setMode(MODE.SLEEPING)}
-              title='Bye'
-            >
-              <img src={srcActive} width={100} alt='cat-image' />
-            </div>
 
-            {mode === MODE.CHATTING ? (
-              <ChatBox />
-            ) : (
-              <>
-                <div>
-                  Hello! My name's Sâu (it's pronounced like "Soul"),
-                  <p>
-                    I'm a cute cat and an AI assistant, trained on the GPT-4
-                    model by my boss Linh
-                  </p>
-                  <p>
-                    Would you like to talk with me? You can ask me anything
-                    about my boss or my capabilities.
-                  </p>
-                </div>
-                <div className='d-flex justify-start'>
-                  <Button
-                    variant='default'
-                    className='yes-button'
-                    onClick={handleSayYes}
-                  >
-                    Yes, I love it
-                  </Button>
-                  <Button
-                    variant='default'
-                    className='mx-2'
-                    onClick={() => setMode(MODE.BYE)}
-                  >
-                    No, I don't like cat
-                  </Button>
-                </div>
-              </>
-            )}
-          </div>
-        </div>
+  return (
+    <>
+      {mode === MODE.CHATTING ? <ChatBox setMode={setMode} /> : null}
+      {mode === MODE.GREETING ? (
+        <GreetingMode setMode={setMode} setGreeted={setGreeted} />
       ) : null}
 
-      {![MODE.BYE, MODE.CHATTING].includes(mode) ? (
+      {[MODE.SLEEPING, MODE.GREETING].includes(mode) ? (
         <div
           className='assistant-button pointer'
           onClick={handleClick}
           title='Cuddle me'
+          role="button"
         >
-          <img src={src} width={70} alt='terminal' />
+          <img width={30} src='chat/star.png' className={mode === MODE.SLEEPING ?  'ai-star-icon' : 'd-none'}/>
+          <Cat width={70} src={verCatSrc} />
         </div>
       ) : null}
-    </div>
+    </>
   );
 }
 
